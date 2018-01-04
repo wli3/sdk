1c6bd1af7fa0348c37d3bf9a561abed268f850a3
William Li <wul@microsoft.com>
2018-01-03 21:41:54 -0800

    WIP

--- src/Tests/Microsoft.NET.ToolPack.Tests/GivenThatWeWantToPackAToolProjectWithExplicitConfig.cs
+++ src/Tests/Microsoft.NET.ToolPack.Tests/GivenThatWeWantToPackAToolProjectWithExplicitConfig.cs
@@ -28,11 +28,35 @@ namespace Microsoft.NET.ToolPack.Tests
         [Fact]
         public void And_explicit_entry_point_name_it_finds_the_entry_point_dll_and_put_in_setting_file()
         {
+            //const string explicitEntry_point_name = "explicit_command_name";
+            //TestAsset helloWorldAsset = _testAssetsManager
+            //.CopyTestAsset("PortableTool", "PackPortableTool" + Path.GetRandomFileName())
+            //.WithSource()
+            //.WithProjectChanges(project =>
+            //{
+            //    var ns = project.Root.Name.Namespace;
+            //    var propertyGroup = project.Root.Elements(ns + "PropertyGroup").First();
+            //    propertyGroup.Add(new XElement("ToolCommandName", "explicit_command_name"));
+            //})
+            //.Restore(Log);
+            //var packCommand = new PackCommand(Log, helloWorldAsset.TestRoot);
+
+            //packCommand.Execute();
+
+            //var nugetPackage = packCommand.GetNuGetPackage();
+            //using (var nupkgReader = new PackageArchiveReader(nugetPackage))
+            //{
+            //    var tmpfilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            //    string copiedFile = nupkgReader.ExtractFile($"tools/DotnetToolSettings.xml", tmpfilePath, null);
+            //    File.ReadAllText(copiedFile)
+            //        .Should()
+            //        .Contain(explicitCommandName);
+            //}
         }
 
 
-        [Fact(Skip = "Pending")]
-        public void And_explicit_commandName_it_finds_commandName_and_put_in_setting_file()
+        [Fact]
+        public void It_finds_commandName_and_put_in_setting_file()
         {
             const string explicitCommandName = "explicit_command_name";
             TestAsset helloWorldAsset = _testAssetsManager
@@ -42,23 +66,21 @@ namespace Microsoft.NET.ToolPack.Tests
             {
                 var ns = project.Root.Name.Namespace;
                 var propertyGroup = project.Root.Elements(ns + "PropertyGroup").First();
-                propertyGroup.Add(new XElement("ToolCommandName", "explicit_command_name"));
+                propertyGroup.Add(new XElement("ToolCommandName", explicitCommandName));
             })
             .Restore(Log);
-
             var packCommand = new PackCommand(Log, helloWorldAsset.TestRoot);
 
             packCommand.Execute();
 
             var nugetPackage = packCommand.GetNuGetPackage();
-
             using (var nupkgReader = new PackageArchiveReader(nugetPackage))
             {
                 var tmpfilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
                 string copiedFile = nupkgReader.ExtractFile($"tools/DotnetToolSettings.xml", tmpfilePath, null);
                 File.ReadAllText(copiedFile)
                     .Should()
-                    .Contain(explicitCommandName);
+                    .Contain($"<Command Name=\"{explicitCommandName}\"");
             }
         }
     }
