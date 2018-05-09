3c3c281a3bd983e202e0fbf6911ef0da5e8fef8a
William Li <wul@microsoft.com>
2018-05-08 18:35:17 -0700

    WIP

--- src/Tasks/Microsoft.NET.Build.Tasks/ResolveAppHostPackageAssets.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ResolveAppHostPackageAssets.cs
@@ -6,21 +6,21 @@ using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using NuGet.ProjectModel;
 
 namespace Microsoft.NET.Build.Tasks
 {
     public sealed class ResolveAppHostPackageAssets : TaskBase
     {
+        private NuGetPackageResolver _packageResolver;
+
         /// <summary>
         /// Path to assets.json.
         /// </summary>
         public string ProjectAssetsFile { get; set; }
 
-        /// <summary>
-        /// Path to assets.cache file.
-        /// </summary>
         [Required]
-        public string ProjectAssetsCacheFile { get; set; }
+        public string DotNetAppHostExecutableNameWithoutExtension { get; set; }
 
         /// <summary>
         /// Path to project file (.csproj|.vbproj|.fsproj)
@@ -35,58 +35,10 @@ namespace Microsoft.NET.Build.Tasks
         public string TargetFrameworkMoniker { get; set; }
 
         [Required]
-        public ITaskItem[] RuntimeIdentifier { get; set; }
+        public string PackagedShimOutputDirectory { get; set; }
 
-        /// <summary>
-        /// Do not write package assets cache to disk nor attempt to read previous cache from disk.
-        /// </summary>
-        public bool DisablePackageAssetsCache { get; set; }
-
-        /// <summary>
-        /// Do not generate transitive project references.
-        /// </summary>
-        public bool DisableTransitiveProjectReferences { get; set; }
-
-        /// <summary>
-        /// Do not add references to framework assemblies as specified by packages.
-        /// </summary>
-        public bool DisableFrameworkAssemblies { get; set; }
-
-        /// <summary>
-        /// Log messages from assets log to build error/warning/message.
-        /// </summary>
-        public bool EmitAssetsLogMessages { get; set; }
-
-        /// <summary>
-        /// Set ExternallyResolved=true metadata on reference items to indicate to MSBuild ResolveAssemblyReferences
-        /// that these are resolved by an external system (in this case nuget) and therefore several steps can be
-        /// skipped as an optimization.
-        /// </summary>
-        public bool MarkPackageReferencesAsExternallyResolved { get; set; }
-
-        /// <summary>
-        /// Project language ($(ProjectLanguage) in common targets -"VB" or "C#" or "F#" ).
-        /// Impacts applicability of analyzer assets.
-        /// </summary>
-        public string ProjectLanguage { get; set; }
-
-        /// <summary>
-        /// Check that there is at least one package dependency in the RID graph that is not in the RID-agnostic graph.
-        /// Used as a heuristic to detect invalid RIDs.
-        /// </summary>
-        public bool EnsureRuntimePackageDependencies { get; set; }
-
-        /// <summary>
-        /// Specifies whether to validate that the version of the implicit platform packages in the assets
-        /// file matches the version specified by <see cref="ExpectedPlatformPackages"/>
-        /// </summary>
-        public bool VerifyMatchingImplicitPackageVersion { get; set; }
-
-        /// <summary>
-        /// Implicitly referenced platform packages.  If set, then an error will be generated if the
-        /// version of the specified packages from the assets file does not match the expected versions.
-        /// </summary>
-        public ITaskItem[] ExpectedPlatformPackages { get; set; }
+        [Required]
+        public ITaskItem[] PackageToolShimRuntimeIdentifiers { get; set; }
 
         [Output]
         public ITaskItem[] RuntimeIdentifierApphost { get; private set; }
@@ -94,40 +46,70 @@ namespace Microsoft.NET.Build.Tasks
         protected override void ExecuteCore()
         {
             var resultPackages = new List<ITaskItem>();
+            NuGet.Frameworks.NuGetFramework targetFramework = NuGetUtils.ParseFrameworkName(TargetFrameworkMoniker);
+            LockFile lockFile = new LockFileCache(this).GetLockFile(ProjectAssetsFile);
+            _packageResolver = NuGetPackageResolver.CreateResolver(lockFile, ProjectPath);
+            LockFileTarget compileTimeTarget = lockFile.GetTargetAndThrowIfNotFound(targetFramework, runtime: null);
 
-            foreach (ITaskItem rid in RuntimeIdentifier)
+            foreach (string rid in PackageToolShimRuntimeIdentifiers.Select(r => r.ItemSpec))
             {
-                var apphostName = "apphost";
-                var resolvePackageAssets = new ResolvePackageAssets
+                var apphostName = DotNetAppHostExecutableNameWithoutExtension;
+
+                if (rid.StartsWith("win"))
                 {
-                    ProjectAssetsFile = Path.GetFullPath(ProjectAssetsFile),
-                    ProjectAssetsCacheFile = Path.GetFullPath(ProjectAssetsCacheFile),
-                    ProjectPath = Path.GetFullPath(ProjectPath),
-                    TargetFrameworkMoniker = TargetFrameworkMoniker,
-                    RuntimeIdentifier = rid.ItemSpec,
-                    DisablePackageAssetsCache = DisablePackageAssetsCache,
-                    DisableTransitiveProjectReferences = DisableTransitiveProjectReferences,
-                    DisableFrameworkAssemblies = DisableFrameworkAssemblies,
-                    EmitAssetsLogMessages = EmitAssetsLogMessages,
-                    MarkPackageReferencesAsExternallyResolved = MarkPackageReferencesAsExternallyResolved,
-                    ProjectLanguage = ProjectLanguage,
-                    EnsureRuntimePackageDependencies = EnsureRuntimePackageDependencies,
-                    VerifyMatchingImplicitPackageVersion = VerifyMatchingImplicitPackageVersion,
-                    ExpectedPlatformPackages = ExpectedPlatformPackages,
-                    BuildEngine = BuildEngine
-                };
-
-                resolvePackageAssets.Execute();
-                ITaskItem[] nativeLibraries = resolvePackageAssets.NativeLibraries;
-                ITaskItem apphostItem = nativeLibraries.Where(t => t.GetMetadata("FileName") == apphostName).Single(); // TODO handling error
-
+                    apphostName += ".exe";
+                }
+                LockFileTarget runtimeTarget = lockFile.GetTargetAndThrowIfNotFound(targetFramework, rid);
                 TaskItem item = new TaskItem(rid);
-                item.SetMetadata("AppHostFilePath", apphostItem.ItemSpec);
+                string resolvedPackageAssetPath = FindApphost(apphostName, runtimeTarget);
+                item.SetMetadata("AppHostFilePath", resolvedPackageAssetPath);
 
                 resultPackages.Add(item);
+                var PackagedShimOutputDirectoryAndRid = Path.Combine(PackagedShimOutputDirectory, rid);
+                Directory.CreateDirectory(PackagedShimOutputDirectoryAndRid);
+                File.Copy(resolvedPackageAssetPath,
+                    Path.Combine(PackagedShimOutputDirectoryAndRid, Path.GetFileName(resolvedPackageAssetPath)),
+                    overwrite: true);
             }
+
             RuntimeIdentifierApphost = resultPackages.ToArray();
         }
+
+        private string FindApphost(string apphostName, LockFileTarget runtimeTarget)
+        {
+            foreach (LockFileTargetLibrary library in runtimeTarget.Libraries)
+            {
+                if (!library.IsPackage())
+                {
+                    continue;
+                }
+
+                foreach (LockFileItem asset in library.NativeLibraries)
+                {
+                    if (asset.IsPlaceholderFile())
+                    {
+                        continue;
+                    }
+
+                    string resolvedPackageAssetPath = ResolvePackageAssetPath(library, asset.Path);
+                    if (Path.GetFileName(resolvedPackageAssetPath) == apphostName)
+                    {
+                        return resolvedPackageAssetPath;
+                    }
+                }
+            }
+
+            throw new BuildErrorException($"Cannot find apphost for {runtimeTarget.RuntimeIdentifier}"); // TODO no check in loc wul
+        }
+
+        private string ResolvePackageAssetPath(LockFileTargetLibrary package, string relativePath)
+        {
+            string packagePath = _packageResolver.GetPackageDirectory(package.Name, package.Version);
+            return Path.Combine(packagePath, NormalizeRelativePath(relativePath));
+        }
+
+        private static string NormalizeRelativePath(string relativePath)
+                => relativePath.Replace('/', Path.DirectorySeparatorChar);
 
     }
 }
