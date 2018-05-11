e4f23867d6496dab967ba532084c639aedafe706
William Li <wul@microsoft.com>
2018-05-11 10:39:46 -0700

    WIP add test for shim

---
+++ src/Tests/Microsoft.NET.TestFramework/Commands/ShimCommand.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Xunit.Abstractions;
+
+namespace Microsoft.NET.TestFramework.Commands
+{
+    public class ShimCommand : TestCommand
+    {
+        private readonly string _commandName;
+
+        public ShimCommand(ITestOutputHelper log, string commandName, params string[] args) : base(log)
+        {
+            _commandName = commandName;
+            Arguments.AddRange(args);
+        }
+
+        protected override SdkCommandSpec CreateCommand(string[] args)
+        {
+            var sdkCommandSpec = new SdkCommandSpec()
+            {
+                FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? _commandName + ".exe" : _commandName,
+                Arguments = args.ToList(),
+                WorkingDirectory = WorkingDirectory
+            };
+
+
+            if (Environment.Is64BitProcess)
+            {
+                sdkCommandSpec.Environment.Add("DOTNET_ROOT", TestContext.Current.ToolsetUnderTest.DotNetHostPath);
+            }
+            else
+            {
+                sdkCommandSpec.Environment.Add("DOTNET_ROOT(x86)", TestContext.Current.ToolsetUnderTest.DotNetHostPath);
+            }
+
+            TestContext.Current.AddTestEnvironmentVariables(sdkCommandSpec);
+            return sdkCommandSpec;
+        }
+    }
+}
--- src/Tests/Microsoft.NET.ToolPack.Tests/GivenThatWeWantToPackAToolProjectWithPackagedShim.cs
+++ src/Tests/Microsoft.NET.ToolPack.Tests/GivenThatWeWantToPackAToolProjectWithPackagedShim.cs
@@ -13,16 +13,24 @@ using Xunit.Abstractions;
 using NuGet.Packaging;
 using System.Xml.Linq;
 using System.Runtime.CompilerServices;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.DotNet.Cli.Utils;
 
 namespace Microsoft.NET.ToolPack.Tests
 {
     public class GivenThatWeWantToPackAToolProjectWithPackagedShim : SdkTest
     {
         private string _testRoot;
+        private string _packageId;
+        private readonly string _packageVersion = "1.0.0";
         private const string _customToolCommandName = "customToolCommandName";
+        private ITestOutputHelper _log;
 
         public GivenThatWeWantToPackAToolProjectWithPackagedShim(ITestOutputHelper log) : base(log)
         {
+            _log = log;
         }
 
         private string SetupNuGetPackage(bool multiTarget, [CallerMemberName] string callingMethod = "")
@@ -36,6 +44,7 @@ namespace Microsoft.NET.ToolPack.Tests
                     XElement propertyGroup = project.Root.Elements(ns + "PropertyGroup").First();
                     propertyGroup.Add(new XElement(ns + "PackageToolShimRuntimeIdentifiers", "win-x64;ubuntu-x64"));
                     propertyGroup.Add(new XElement(ns + "ToolCommandName", _customToolCommandName));
+                    propertyGroup.Add(new XElement(ns + "PackageId", _customToolCommandName));
 
                     if (multiTarget)
                     {
@@ -50,6 +59,7 @@ namespace Microsoft.NET.ToolPack.Tests
             var packCommand = new PackCommand(Log, helloWorldAsset.TestRoot);
 
             packCommand.Execute();
+            _packageId = Path.GetFileNameWithoutExtension(packCommand.ProjectFile);
 
             return packCommand.GetNuGetPackage();
         }
@@ -107,6 +117,61 @@ namespace Microsoft.NET.ToolPack.Tests
                         "RID should be the excat match of the property, even Apphost only has explicitly win, osx and linux");
                 }
             }
+        }
+
+        [WindowsOnlyTheory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void It_produce_valid_shims(bool multiTarget)
+        {
+            if (!Environment.Is64BitOperatingSystem)
+            {
+                // only sample test on win-x64 since shims are RID specific
+                return;
+            }
+            var nugetPackage = SetupNuGetPackage(multiTarget);
+            using (var nupkgReader = new PackageArchiveReader(nugetPackage))
+            {
+                IEnumerable<NuGet.Frameworks.NuGetFramework> supportedFrameworks = nupkgReader.GetSupportedFrameworks();
+                supportedFrameworks.Should().NotBeEmpty();
+                var tmpfilePathRoot = Path.Combine(_testRoot, "temp", Path.GetRandomFileName());
+
+                foreach (NuGet.Frameworks.NuGetFramework framework in supportedFrameworks)
+                {
+                    CopyPackageAssetToToolLayout("consoledemo.runtimeconfig.json", nupkgReader, tmpfilePathRoot, framework);
+                    CopyPackageAssetToToolLayout("consoledemo.deps.json", nupkgReader, tmpfilePathRoot, framework);
+                    CopyPackageAssetToToolLayout("try-toolrid.dll", nupkgReader, tmpfilePathRoot, framework);
+
+                    string shimPath = Path.Combine(tmpfilePathRoot, $"{_customToolCommandName}.exe");
+                    nupkgReader.ExtractFile(
+                        $"tools/{framework.GetShortFolderName()}/any/shims/win-x64/{_customToolCommandName}.exe",
+                        shimPath,
+                        null);
+
+                    var command = new ShimCommand(_log, _customToolCommandName);
+
+                    command.Execute().Should()
+                      .Pass()
+                      .And
+                      .HaveStdOutContaining("Hello World!");
+                }
+            }
+
+
+            //tring copiedFile = nupkgReader.ExtractFile($"tools/{anyTfm}/any/DotnetToolSettings.xml", tmpfilePath, null);
+        }
+
+        private void CopyPackageAssetToToolLayout(
+            string nupkgAssetName,
+            PackageArchiveReader nupkgReader,
+            string tmpfilePathRoot, 
+            NuGet.Frameworks.NuGetFramework framework)
+        {
+            var toolLayoutDirectory = 
+                Path.Combine(tmpfilePathRoot, ".store", _packageId, _packageVersion, _packageId, _packageVersion, "tools", framework.GetShortFolderName(), "any");
+            var destinationFilePath = 
+                Path.Combine(toolLayoutDirectory, nupkgAssetName);
+            var copiedFile = nupkgReader.ExtractFile($"tools/{framework.GetShortFolderName()}/any/{nupkgAssetName}", destinationFilePath, null);
         }
     }
 }
