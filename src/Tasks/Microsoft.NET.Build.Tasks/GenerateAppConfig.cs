e0955803569749f3ed3bf5355b7574ae5f163b3f
William Li <wul@microsoft.com>
2018-07-29 14:45:02 -0700

    Boilerplate.cs

---
+++ src/Tasks/Microsoft.NET.Build.Tasks/GenerateAppConfig.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Frameworks;
+using NuGet.ProjectModel;
+
+namespace Microsoft.NET.Build.Tasks
+{
+    public sealed class GenerateAppConfig : TaskBase
+    {
+
+        [Required]
+        public string AppConfigFile { get; set; }
+
+        [Required]
+        public string OutputAppConfigFile { get; set; }
+
+        protected override void ExecuteCore()
+        {
+            
+           
+        }
+
+
+    }
+}
