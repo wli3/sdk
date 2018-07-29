5808383a5174d837737618920cfb7935fdd27f22
William Li <wul@microsoft.com>
2018-07-29 13:35:51 -0700

    Generate appconfig for full framework project

---
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenAGenerateAppConfig.cs
@@ -0,0 +1,143 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using NuGet.Frameworks;
+using System.Linq;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Microsoft.NET.Build.Tasks.UnitTests
+{
+    public class GivenAGenerateAppConfig
+    {
+        [Fact]
+        public void It_creates_startup_and_supportedRuntime_nod_when_there_is_not_any()
+        {
+            var doc =
+                    new XDocument(
+                        new XDeclaration("1.0", "utf-8", "true"),
+                        new XElement("configuration"));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, "net452");
+
+            doc.Element("configuration")
+                .Elements("startup")
+                .Single().Elements()
+                .Should().Contain(e => e.Name.LocalName == "supportedRuntime");
+        }
+
+        [Fact]
+        public void It_creates_supportedRuntime_nod_when_there_is_startup()
+        {
+            var doc =
+                    new XDocument(
+                        new XDeclaration("1.0", "utf-8", "true"),
+                        new XElement("configuration", new XElement("startup")));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, "net452");
+
+            doc.Element("configuration")
+                .Elements("startup")
+                .Single().Elements()
+                .Should().Contain(e => e.Name.LocalName == "supportedRuntime");
+        }
+
+        [Fact]
+        public void It_does_not_change_supportedRuntime_nod_when_there_is_supportedRuntime()
+        {
+            var doc =
+                    new XDocument(
+                        new XDeclaration("1.0", "utf-8", "true"),
+                        new XElement("configuration",
+                            new XElement("startup",
+                                new XElement("supportedRuntime",
+                                    new XAttribute("version", "v4.0"),
+                                    new XAttribute("sku", ".NETFramework,Version=v4.7.2")))));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, "net461");
+
+            XElement supportedRuntime = doc.Element("configuration")
+                .Elements("startup")
+                .Single().Elements("supportedRuntime")
+                .Single();
+
+            supportedRuntime.Should().HaveAttribute("version", "v4.0");
+            supportedRuntime.Should().HaveAttribute("sku", ".NETFramework,Version=v4.7.2");
+        }
+
+        // intersection of https://docs.microsoft.com/en-us/nuget/reference/target-frameworks
+        // and https://docs.microsoft.com/en-us/dotnet/framework/configure-apps/file-schema/startup/supportedruntime-element#version
+        [Theory]
+        [InlineData("net11", "v1.1.4322")]
+        [InlineData("net20", "v2.0.50727")]
+        [InlineData("net35", "v2.0.50727")]
+        public void It_Generate_correct_version_and_sku_for_below40(string targetframework, string expectedVersion)
+        {
+            var doc =
+                    new XDocument(
+                        new XDeclaration("1.0", "utf-8", "true"),
+                        new XElement("configuration"));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, targetframework);
+
+            XElement supportedRuntime = doc.Element("configuration")
+                .Elements("startup")
+                .Single().Elements("supportedRuntime")
+                .Single();
+
+            supportedRuntime.Should().HaveAttribute("version", expectedVersion);
+            supportedRuntime.Attribute("sku").Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("net45", "v4.0", ".NETFramework,Version=v4.5")]
+        [InlineData("net451", "v4.0", ".NETFramework,Version=v4.5.1")]
+        [InlineData("net452", "v4.0", ".NETFramework,Version=v4.5.2")]
+        [InlineData("net46", "v4.0", ".NETFramework,Version=v4.6")]
+        [InlineData("net461", "v4.0", ".NETFramework,Version=v4.6.1")]
+        [InlineData("net462", "v4.0", ".NETFramework,Version=v4.6.2")]
+        [InlineData("net47", "v4.0", ".NETFramework,Version=v4.7")]
+        [InlineData("net471", "v4.0", ".NETFramework,Version=v4.7.1")]
+        [InlineData("net472", "v4.0", ".NETFramework,Version=v4.7.2")]
+        public void It_Generate_correct_version_and_sku_for_above40(string targetframework, string expectedVersion, string expectedSku)
+        {
+            var targetFrameworkParsed = NuGetFramework.Parse(targetframework);
+
+            var doc =
+                new XDocument(
+                    new XDeclaration("1.0", "utf-8", "true"),
+                    new XElement("configuration"));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, targetframework);
+
+            XElement supportedRuntime = doc.Element("configuration")
+                .Elements("startup")
+                .Single().Elements("supportedRuntime")
+                .Single();
+
+            supportedRuntime.Should().HaveAttribute("version", expectedVersion);
+            supportedRuntime.Should().HaveAttribute("sku", expectedSku);
+        }
+
+        [Theory]
+        [InlineData("net10")]
+        [InlineData("net30")]
+        [InlineData("net999")]
+        [InlineData("netstandard20")]
+        public void It_does_not_generate_version_and_sku_for_non_supported(string targetframework)
+        {
+            var targetFrameworkParsed = NuGetFramework.Parse(targetframework);
+
+            var doc =
+                new XDocument(
+                    new XDeclaration("1.0", "utf-8", "true"),
+                    new XElement("configuration"));
+
+            GenerateAppConfig.AddSupportedRuntimeToAppconfigFile(doc, targetframework);
+
+            doc.Element("configuration")
+                .Elements("startup").Should().BeNullOrEmpty();
+        }
+    }
+}
---
+++ src/Tasks/Microsoft.NET.Build.Tasks/GenerateAppConfig.cs
@@ -0,0 +1,166 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Build.Framework;
+using NuGet.Frameworks;
+
+namespace Microsoft.NET.Build.Tasks
+{
+    public sealed class GenerateAppConfig : TaskBase
+    {
+        /// <summary>
+        /// Path to the app.config source file.
+        /// </summary>
+        public ITaskItem AppConfigFile { get; set; }
+
+        /// <summary>
+        /// Name of the output application config file: $(TargetFileName).config
+        /// </summary>
+        public string TargetName { get; set; }
+
+        public string TargetFramework { get; set; }
+
+        /// <summary>
+        /// Path to an intermediate file where we can write the input app.config plus the generated startup supportedRuntime
+        /// </summary>
+        [Output]
+        public ITaskItem OutputAppConfigFile { get; set; }
+
+        protected override void ExecuteCore()
+        {
+            XDocument doc = LoadAppConfig(AppConfigFile);
+
+            AddSupportedRuntimeToAppconfigFile(doc, TargetFramework);
+
+            if (File.Exists(OutputAppConfigFile.ItemSpec))
+            {
+                File.Delete(OutputAppConfigFile.ItemSpec);
+            }
+
+            if (AppConfigFile != null)
+            {
+                AppConfigFile.CopyMetadataTo(OutputAppConfigFile);
+            }
+            else
+            {
+                OutputAppConfigFile.SetMetadata(MetadataKeys.TargetPath, TargetName);
+            }
+
+            var fileStream = new FileStream(
+                OutputAppConfigFile.ItemSpec,
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.Read);
+            using (var stream = new StreamWriter(fileStream))
+            {
+                doc.Save(stream);
+            }
+        }
+
+        public static void AddSupportedRuntimeToAppconfigFile(XDocument doc, string targetframework)
+        {
+            XElement startupNode = doc.Root
+                                      .Nodes()
+                                      .OfType<XElement>()
+                                      .FirstOrDefault(e => e.Name.LocalName == "startup");
+
+            string runtimeVersion = string.Empty;
+            if (!HasExistingSupportedRuntime(startupNode))
+            {
+                if (TryGetSupportRuntimeNode(
+                    targetframework,
+                    runtimeVersion,
+                    out XElement supportedRuntime))
+                {
+                    if (startupNode == null)
+                    {
+                        startupNode = new XElement("startup");
+                        doc.Root.Add(startupNode);
+                    }
+
+                    startupNode.Add(supportedRuntime);
+                }
+            }
+        }
+
+        private static bool HasExistingSupportedRuntime(XElement startupNode)
+        {
+            return startupNode != null
+                  && startupNode.Nodes().OfType<XElement>().Any(e => e.Name.LocalName == "supportedRuntime");
+        }
+
+        //https://github.com/dotnet/docs/blob/master/docs/framework/configure-apps/file-schema/startup/supportedruntime-element.md
+        private static bool TryGetSupportRuntimeNode(string targetframework, string runtimeVersion, out XElement supportedRuntime)
+        {
+            supportedRuntime = null;
+            var targetFrameworkParsed = NuGetFramework.Parse(targetframework);
+            if (targetFrameworkParsed.Framework == ".NETFramework")
+            {
+                if (targetFrameworkParsed.Version.Major < 4)
+                {
+                    if (_targetFrameworkBelow40RuntimeVersionMap.TryGetValue(targetFrameworkParsed.GetShortFolderName(), out string value))
+                    {
+                        runtimeVersion = value;
+                        supportedRuntime =
+                            new XElement(
+                                "supportedRuntime",
+                                new XAttribute("version", value));
+
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (targetFrameworkParsed.Version.Major == 4)
+                {
+                    supportedRuntime =
+                        new XElement(
+                            "supportedRuntime",
+                            new XAttribute("version", "v4.0"),
+                                new XAttribute("sku", targetFrameworkParsed.DotNetFrameworkName));
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Load or create App.Config
+        /// </summary>
+        private XDocument LoadAppConfig(ITaskItem appConfigItem)
+        {
+            XDocument document;
+            if (appConfigItem == null)
+            {
+                document =
+                    new XDocument(
+                        new XDeclaration("1.0", "utf-8", "true"),
+                            new XElement("configuration"));
+            }
+            else
+            {
+                document = XDocument.Load(appConfigItem.ItemSpec);
+                if (document.Root == null || document.Root.Name != "configuration")
+                {
+                    throw new BuildErrorException(Strings.AppConfigRequiresRootConfiguration);
+                }
+            }
+
+            return document;
+        }
+
+        private static readonly Dictionary<string, string> _targetFrameworkBelow40RuntimeVersionMap = new Dictionary<string, string>()
+        {
+            ["net11"] = "v1.1.4322",
+            ["net20"] = "v2.0.50727",
+            ["net35"] = "v2.0.50727",
+        };
+    }
+}
