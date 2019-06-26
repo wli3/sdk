119372dd39d5d86c571e15f3382472d91f9a89fb
William Li <wul@microsoft.com>
2019-06-26 14:04:38 -0700

    Also remove that in sh

--- eng/restore-toolset.sh
+++ eng/restore-toolset.sh
@@ -10,8 +10,6 @@ function InitializeCustomSDKToolset {
   fi
 
   InitializeDotNetCli true
-  InstallDotNetSharedFramework "1.0.5"
-  InstallDotNetSharedFramework "1.1.2"
   InstallDotNetSharedFramework "2.1.0"
   InstallDotNetSharedFramework "2.2.5"
 }
