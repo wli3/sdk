184d12ee1599e117c7bdb209f94e4e4b2da3a6f7
William Li <wul@microsoft.com>
2019-08-22 15:03:15 -0700

    Add blocking exe

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildACppCliAppProject.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildACppCliAppProject.cs
@@ -2,11 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
-using System.Linq;
 using FluentAssertions;
+using Microsoft.NET.Build.Tasks;
 using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.NET.Build.Tests
@@ -18,34 +19,17 @@ namespace Microsoft.NET.Build.Tests
         }
 
         [FullMSBuildOnlyFact]
-        public void It_builds_and_runs()
+        public void It_should_fail_with_error_message()
         {
             var testAsset = _testAssetsManager
                 .CopyTestAsset("NETCoreCppClApp")
-                .WithSource()
-                .Restore(Log, "NETCoreCppCliTest.sln");
+                .WithSource();
 
             new BuildCommand(Log, Path.Combine(testAsset.TestRoot, "NETCoreCppCliTest.sln"))
-                .Execute("-p:Platform=x64")
+                .Execute("/restore")
                 .Should()
-                .Pass();
-
-            // There is a bug in MSVC in CI's old VS image.
-            // Once https://github.com/dotnet/core-eng/issues/7409/ is done
-            // we should directly run the app to test.
-
-            // TODO wul remove that before check in
-            var exe = Path.Combine(
-                testAsset.TestRoot,
-                "x64",
-                "Debug",
-                "NETCoreCppCliTest.exe");
-
-            new RunExeCommand(Log, exe).Execute()
-                .Should()
-                .Pass()
-                .And
-                .HaveStdOutContaining("Hello world from C++/CLI");
+                .Fail()
+                .And.HaveStdOutContaining(Strings.NoSupportCppExeDotnetCore);
         }
     }
 }
