f9f1919b739b4dcdded38c8f89cfb96477eb104b
William Li <wul@microsoft.com>
2020-07-15 11:33:33 -0700

    WIP

---
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/CheckForUnsupportedWindowsTargetPlatformVersionTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.NET.Build.Tasks.UnitTests
+{
+    public class CheckForUnsupportedWindowsTargetPlatformVersionTests
+    {
+        private MockTaskItem _validWindowsSDKKnownFrameworkReference
+            = new MockTaskItem("Microsoft.Windows.SDK.NET.Ref",
+                new Dictionary<string, string>
+                {
+                    {"TargetFramework", "net5.0-windows10.0.18362"},
+                    {"RuntimeFrameworkName", "Microsoft.Windows.SDK.NET.Ref"},
+                    {"DefaultRuntimeFrameworkVersion", "10.0.18362.1-preview"},
+                    {"LatestRuntimeFrameworkVersion", "10.0.18362.1-preview"},
+                    {"TargetingPackName", "Microsoft.Windows.SDK.NET.Ref"},
+                    {"TargetingPackVersion", "10.0.18362.1-preview"},
+                    {"RuntimePackNamePatterns", "Microsoft.Windows.SDK.NET.Ref"},
+                    {"RuntimePackRuntimeIdentifiers", "any"},
+                    {MetadataKeys.RuntimePackAlwaysCopyLocal, "true"},
+                    {"IsWindowsOnly", "true"},
+                });
+
+        [Fact]
+        public void Given_matching_TargetPlatformVersion_it_should_pass()
+        {
+            const string minimalRuntimeGraphPathContent =
+                "{\"runtimes\":{\"any\":{\"#import\":[\"base\"]},\"base\":{\"#import\":[]}}}";
+            var runtimeGraphPathPath = Path.GetTempFileName();
+            File.WriteAllText(runtimeGraphPathPath, minimalRuntimeGraphPathContent);
+
+            var task = new CheckForUnsupportedWindowsTargetPlatformVersion
+            {
+                BuildEngine = new MockNeverCacheBuildEngine4(),
+                TargetPlatformVersion = "10.0.18362",
+                KnownFrameworkReferences = new[]
+                {
+                    new MockTaskItem("Microsoft.Windows.SDK.NET.Ref",
+                        new Dictionary<string, string>
+                        {
+                            {"TargetFramework", "net5.0-windows10.0.17760"},
+                            {"RuntimeFrameworkName", "Microsoft.Windows.SDK.NET.Ref"},
+                            {"DefaultRuntimeFrameworkVersion", "10.0.17760.1-preview"},
+                            {"LatestRuntimeFrameworkVersion", "10.0.17760.1-preview"},
+                            {"TargetingPackName", "Microsoft.Windows.SDK.NET.Ref"},
+                            {"TargetingPackVersion", "10.0.17760.1-preview"},
+                            {"RuntimePackNamePatterns", "Microsoft.Windows.SDK.NET.Ref"},
+                            {"RuntimePackRuntimeIdentifiers", "any"},
+                            {MetadataKeys.RuntimePackAlwaysCopyLocal, "true"},
+                            {"IsWindowsOnly", "true"},
+                        }),
+                    _validWindowsSDKKnownFrameworkReference,
+                }
+            };
+
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                task.Execute().Should().BeFalse("IsWindowsOnly=true");
+                return;
+            }
+
+            task.Execute().Should().BeTrue();
+        }
+
+    }
+}
---
+++ src/Tasks/Microsoft.NET.Build.Tasks/CheckForUnsupportedWindowsTargetPlatformVersion.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Frameworks;
+
+namespace Microsoft.NET.Build.Tasks
+{
+    public class CheckForUnsupportedWindowsTargetPlatformVersion : TaskBase
+    {
+        public ITaskItem[] KnownFrameworkReferences { get; set; } = Array.Empty<ITaskItem>();
+
+        public string TargetPlatformVersion { get; set; }
+
+        public CheckForUnsupportedWindowsTargetPlatformVersion()
+        {
+        }
+
+        protected override void ExecuteCore()
+        {
+        }
+    }
+
+    internal struct KnownFrameworkReference
+    {
+        ITaskItem _item;
+        public KnownFrameworkReference(ITaskItem item)
+        {
+            _item = item;
+            TargetFramework = new NuGetFrameworkTemp(item.GetMetadata("TargetFramework"));
+        }
+
+        //  The name / itemspec of the FrameworkReference used in the project
+        public string Name => _item.ItemSpec;
+
+        //  The framework name to write to the runtimeconfig file (and the name of the folder under dotnet/shared)
+        public string RuntimeFrameworkName => _item.GetMetadata(MetadataKeys.RuntimeFrameworkName);
+        public string DefaultRuntimeFrameworkVersion => _item.GetMetadata("DefaultRuntimeFrameworkVersion");
+
+        //  The ID of the targeting pack NuGet package to reference
+        public string TargetingPackName => _item.GetMetadata("TargetingPackName");
+        public string TargetingPackVersion => _item.GetMetadata("TargetingPackVersion");
+        public string TargetingPackFormat => _item.GetMetadata("TargetingPackFormat");
+
+        public string RuntimePackRuntimeIdentifiers => _item.GetMetadata(MetadataKeys.RuntimePackRuntimeIdentifiers);
+
+        public bool IsWindowsOnly => _item.HasMetadataValue("IsWindowsOnly", "true");
+
+        public bool RuntimePackAlwaysCopyLocal =>
+            _item.HasMetadataValue(MetadataKeys.RuntimePackAlwaysCopyLocal, "true");
+
+        public string Profile => _item.GetMetadata("Profile");
+
+        public NuGetFrameworkTemp TargetFramework { get; }
+
+
+        // TODO replace with the proper impl from nuget
+        internal class NuGetFrameworkTemp
+        {
+            public NuGetFrameworkTemp(string targetFramework)
+            {
+                Match match = Regex.Match(targetFramework, "([^-]+)");
+                var beforeDash = match.Value;
+
+                var nuGetFramework = NuGetFramework.Parse(beforeDash);
+                Framework = nuGetFramework.Framework;
+                Version = nuGetFramework.Version;
+                ShortFolderName = nuGetFramework.GetShortFolderName();
+
+                if (beforeDash != targetFramework)
+                {
+                    var afterDash = targetFramework.Substring(match.Length + 1);
+                    Match matchPlatform = Regex.Match(afterDash, "^[A-Za-z]+");
+                    Platform = matchPlatform.Value;
+                    PlatformVersion = afterDash.Substring(matchPlatform.Length);
+                }
+            }
+
+            public string Framework { get; }
+            public Version Version { get; }
+            public string Platform { get; }
+
+            public string PlatformVersion { get; }
+
+            private string ShortFolderName { get; }
+
+            public string GetShortFolderName()
+            {
+                return ShortFolderName;
+            }
+        }
+    }
+}
