e1abe654431d896b6a0476523454c2cd31ac68ae
William Li <wul@microsoft.com>
2020-08-17 21:47:37 -0700

    scaffolding

---
+++ src/Layout/toolset-tasks/OverrideAndCreateBundledNETCoreAppPackageVersion.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /// <summary>
+    /// Use the runtime in dotnet/sdk instead of in the stage 0 to avoid circualr dependency.
+    /// If there is a change dependend on the latest runtime. Without override the runtime version in BundledNETCoreAppPackageVersion
+    /// we would need to somehow get this change in without the test, and then insertion dotnet/installer
+    /// and then update the stage 0 back.
+    /// 
+    /// Use a task to override since it was generated as a string literal replace anyway.
+    /// And using C# can have better error when anything goes wrong.
+    /// </summary>
+    public sealed class OverrideAndCreateBundledNETCoreAppPackageVersion : Task
+    {
+        [Required]
+        public string Stage0MicrosoftNETCoreAppRefPackageVersionPath { get; set; }
+
+        [Required]
+        public string MicrosoftNETCoreAppRefPackageVersion { get; set; }
+
+        [Required]
+        public string OutputPath { get; set; }
+
+        public override bool Execute()
+        {
+            return ExecuteInternal(Stage0MicrosoftNETCoreAppRefPackageVersionPath, MicrosoftNETCoreAppRefPackageVersion, OutputPath);
+        }
+
+        public static bool ExecuteInternal(string stage0MicrosoftNETCoreAppRefPackageVersionPath, string microsoftNETCoreAppRefPackageVersion, string outputPath)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Tests/toolset-tasks.Tests/MockFileSystemTests.cs
@@ -0,0 +1,22 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Microsoft.DotNet.Build.Tasks;
+using Xunit;
+
+namespace Microsoft.DotNet.Cli.Build.Tests
+{
+    public class OverrideAndCreateBundledNETCoreAppPackageVersionTests
+    {
+        [Fact]
+        public void AppleSauce()
+        {
+            OverrideAndCreateBundledNETCoreAppPackageVersion.ExecuteInternal("", "", "").Should().BeTrue();
+        }
+    }
+}
